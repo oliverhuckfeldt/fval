6735186efe580c692e744ca4da2521f4227c2218
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2021-12-27 04:04:07 +0100

    Prevents the form from being autovalidated and submitted.

--- fval.js
+++ fval.js
@@ -4,8 +4,15 @@ class FVal {
 
     constructor(formId) {
         const form = document.getElementById(formId);
+
         if (!form)
             throw new Error('Form ID invalid or does not exist.');
+
         this.#formElement = form;
+        this.#formElement.setAttribute('novalidate', '');
+        this.#formElement.addEventListener('submit', (event) => {
+            console.log('Form is submitted!');
+            event.preventDefault();
+        });
     }
 }
