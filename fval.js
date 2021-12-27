0ef453334258f68d82088c0d283c81e3a097921d
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2021-12-27 04:37:29 +0100

    Added textarea. Iterates over form fields on submit and checks for registered  node types.

--- fval.js
+++ fval.js
@@ -1,6 +1,7 @@
 class FVal {
 
     #formElement;
+    #fieldTypes = ['input', 'textarea']
 
     constructor(formId) {
         const form = document.getElementById(formId);
@@ -11,8 +12,15 @@ class FVal {
         this.#formElement = form;
         this.#formElement.setAttribute('novalidate', '');
         this.#formElement.addEventListener('submit', (event) => {
-            console.log('Form is submitted!');
             event.preventDefault();
+
+            const elements = this.#formElement.elements;
+            const fieldTypes = this.#fieldTypes;
+
+            for (let i = 0; i < elements.length; i++) {
+                if (fieldTypes.indexOf(elements[i].nodeName.toLowerCase()) !== -1)
+                    console.log(elements[i].nodeName);
+            }
         });
     }
 }
